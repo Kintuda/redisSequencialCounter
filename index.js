7734aa524154746c903050668329d518e6b7aa0c
Kin <kin@localhost.localdomain>
2019-02-02 20:57:42 -0500

    Final build

--- index.js
+++ index.js
@@ -2,100 +2,60 @@ require('dotenv').config()
 const redisDatabase = require('./db/connection')
 let env = process.env
 let redis
-let circuitOpen = false
 
-async function initialValue(id, retries){
+async function transactionIncrement(key,id, retries) {
   try {
-    await redis.watch(`santander.${env.NODE_ENV}.${id}`)
-    let data = await redis.get(`santander.${env.NODE_ENV}.${id}`)
-    if(retries === 0){
-      throw new Error('Erro ao definir NSU')
-    }
-    if(!data){
-      console.log('From zero')
-      let result = await redis.multi()
-        .set(`santander.${env.NODE_ENV}.${id}`, 1)
-        .exec()
-      if(!result){
-        return await initialValue(id, retries - 1)
-      }
-      return 1
-    }else{
-      console.log('Increment');
-      return await transactionIncrement(id, retries)
-    }
-  } catch (error) {
-    circuitOpen = true
-    console.log(error)
-    throw error
-  }
-}
-async function transactionIncrement(id, retries) {
-  try {
-    // await redis.watch(`santander.${env.NODE_ENV}.${id}`)
+    await redis.watch(key)
     let result = await redis.multi()
-                        .incr(`santander.${env.NODE_ENV}.${id}`)
-                        .get(`santander.${env.NODE_ENV}.${id}`)
-                        .exec()
+                  .incr(key)
+                  .get(key)
+                  .exec()
 
     if(result === null){
+      console.log(`Value changed while incrementing`)
       return await transactionIncrement(id,retries-1)
     }
 
     if(retries === 0){
-      throw new Error(`Erro ao solicitar nsu`)
+      throw new Error('Reached the max number of retries')
     }
                        
     return result[0][1]
   } catch (error) {
-    circuitOpen = true
+    console.log(`Error while incrementing key ${error}`)
     throw error
   }
-                      
-
-
-}
-
-function stringBuilder(id) {
-  return `santander.${env.NODE_ENV}.${id}`
 }
 
 async function connect() {
   try {
     redis = redisDatabase()
   } catch (error) {
-    circuitOpen = true
-    console.log(error)
+    console.log('Error while connecting into instance of REDIS database')
     throw error
   }
 }
 
-async function main(id) {
+//testing
+async function testLoad(id) {
   try {
     await connect()
-    // await redis.del(`santander.${env.NODE_ENV}.1`)
-    // const data = await getData(1)
-    // let data = await redis.get(`santander.${env.NODE_ENV}.${id}`)
-    const string = stringBuilder(1)
-    let testes = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,0]
-    // console.log(teste + ` TESTE ${id}`);
-    return await Promise.all(testes.map(test=> initialValue(1, 50)))
-    // console.log('done');
-    // return teste
+    let paralelCalling = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,0]
+    return await Promise.all(paralelCalling.map(test=> transactionIncrement(`teste${id}`, 1, 50)))
   } catch (error) {
-    console.log(error);
+    console.log(`Error, ${error}`)
   }
 }
+
 let array = []
-main(1).then(res=>console.log(res))
-main(2).then(res=>console.log(res))
-main(3).then(res=>console.log(res))
-main(4).then(res=>console.log(res))
-main(5).then(res=>console.log(res))
-main(6).then(res=>console.log(res))
-main(7).then(res=>console.log(res))
-main(8).then(res=>console.log(res))
-// Promise.all(array).then(res=>console.log(res))
+testLoad(1).then(res=>console.log(res))
+testLoad(1).then(res=>console.log(res))
+testLoad(1).then(res=>console.log(res))
+testLoad(1).then(res=>console.log(res))
+testLoad(1).then(res=>console.log(res))
+testLoad(1).then(res=>console.log(res))
+testLoad(1).then(res=>console.log(res))
+testLoad(1).then(res=>console.log(res))
 
 
 
